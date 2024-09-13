2aa814fa1f622162bba61be91a97f98e1984e3ce
Abdullah801953 <ak786shariq@gmail.com>
2024-09-13 18:19:18 +0530

    nextjs tutorial

---
+++ hello-world/src/app/blog/first/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1>First blog post</h1>
+  )
+}
+
+export default page
---
+++ hello-world/src/app/profile/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1>My profile</h1>
+  )
+}
+export default page;
